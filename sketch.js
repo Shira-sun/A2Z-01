9423a45a0e33ab41a04f083a464654b2e51ae61e
Shira-sun <shira@seri-levi.com>
2021-09-14 10:11:16 -0400

    added html/css/p5

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let poem = ["some", "thing", "for", "some", "one"];
+let ind;
+let x;
+let y;
+let xoff1 = 0;
+let xoff2 = 1000;
+
+function setup() {
+  createCanvas(800, 800);
+  background(200);
+}
+
+function draw() {
+  ind = floor(map(noise(xoff1), 0, 1, 0, 6));
+
+  x = map(noise(xoff1), 0, 1, 0, width);
+  y = map(noise(xoff2), 0, 1, 0, height);
+
+  xoff1 += 0.01;
+  xoff2 += 0.01;
+
+  console.log(ind);
+  blendMode(OVERLAY);
+  textSize(32);
+  text(poem[ind], x, y);
+}
