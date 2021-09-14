c7b04c3fe95db611a3410981a63fa0a1836d3d88
Shira-sun <shira@seri-levi.com>
2021-09-14 17:21:46 -0400

    added buttons

--- sketch.js
+++ sketch.js
@@ -4,10 +4,36 @@ let x;
 let y;
 let xoff1 = 0;
 let xoff2 = 1000;
+let button;
+let h1;
 
 function setup() {
   createCanvas(800, 800);
-  background(100);
+  background(200);
+  resetSketch();
+
+  //button
+  h1 = createP("restart");
+  h1.position(30, 70);
+  h1.style("color:black");
+
+  button = createButton("⟲");
+  button.position(30, 110);
+  button.mousePressed(resetSketch);
+  button.style("color:black");
+
+  h1 = createP("Start/Stop");
+  h1.position(730, 70);
+  h1.style("color:black");
+
+  pause = createButton("▹");
+  pause.position(760, 110);
+  pause.mousePressed(resetSketch);
+  pause.style("color:black");
+}
+
+function resetSketch() {
+  //draw();
 }
 
 function draw() {
@@ -22,8 +48,8 @@ function draw() {
   console.log(ind);
   blendMode(OVERLAY);
   textSize(32);
+  fill(00);
   text(poem[ind], x, y);
-
   // adding a comment here
   //first git push
 }
