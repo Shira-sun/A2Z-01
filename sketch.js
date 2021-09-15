2555e9f9278fd69f8e171ae4bd52e18bfaf95984
Shira-sun <shira@seri-levi.com>
2021-09-15 16:24:25 -0400

    restart BG

--- sketch.js
+++ sketch.js
@@ -8,31 +8,34 @@ let button;
 let h1;
 
 function setup() {
-  createCanvas(800, 800);
-  background(200);
   resetSketch();
+  noLoop();
 
   //button
   h1 = createP("restart");
-  h1.position(30, 70);
+  h1.position(30, 100);
   h1.style("color:black");
 
   button = createButton("⟲");
-  button.position(30, 110);
+  button.position(30, 140);
   button.mousePressed(resetSketch);
   button.style("color:black");
 
   h1 = createP("Start/Stop");
-  h1.position(730, 70);
+  h1.position(730, 100);
   h1.style("color:black");
 
   pause = createButton("▹");
-  pause.position(760, 110);
+  pause.position(760, 140);
   pause.mousePressed(resetSketch);
   pause.style("color:black");
 }
 
 function resetSketch() {
+  createCanvas(800, 800);
+  background(200);
+  loop();
+
   //draw();
 }
 
